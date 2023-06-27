21d46860513599ccb51db05c65dae04ab87bfe0a
Hila Amsterdam <128729989+HillaAmsterdam@users.noreply.github.com>
2023-06-27 16:24:54 +0300

    nav bar,product type,products

--- src/models/cart.js
+++ src/models/cart.js
@@ -9,13 +9,10 @@ const cartScheme = new mongoose.Schema({
     type: String,
     required: true,
   },
-<<<<<<< HEAD
   productprice: {
     type: Number,
     required: true,
   },
-=======
->>>>>>> 0acbe3a4bb962c649547d3a106e5e25de698bfbd
 });
 
 const Cart = mongoose.model("cart", cartScheme);
