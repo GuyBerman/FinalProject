11aef5d175d70a9a4f03a65a58268594b3977921
NadavBenShachar <128729979+NadavBenShachar@users.noreply.github.com>
2023-08-12 18:15:25 +0300

    delete users in admin panel

--- src/public/adminusers.js
+++ src/public/adminusers.js
@@ -1,82 +1,79 @@
 const fetchimages = async () => {
-  const res = await fetch("/api/getUser");
-  const User = await res.json();
-  const user = User.map((item) => {
-    return {
-      email: item.email,
-      firstname: item.firstname,
-      lastname: item.lastname,
-      password: item.password,
-      admin: item.admin,
-      cart: item.cart,
-    };
-  });
-
-  user.map((user) => {
+  try {
+    const res = await fetch("/api/getUser");
+    const User = await res.json();
     const table = document.getElementById("users"); // Get the table element
-    const tr = document.createElement("tr");
 
-    // Create <td> elements for each data element
-    const emailCell = document.createElement("td");
-    emailCell.innerHTML = user.email;
-    tr.appendChild(emailCell);
+    User.forEach((item) => {
+      const tr = document.createElement("tr");
 
-    const firstnameCell = document.createElement("td");
-    firstnameCell.innerHTML = user.firstname;
-    tr.appendChild(firstnameCell);
+      const emailCell = document.createElement("td");
+      emailCell.innerHTML = item.email;
+      tr.appendChild(emailCell);
 
-    const lastnameCell = document.createElement("td");
-    lastnameCell.innerHTML = user.lastname;
-    tr.appendChild(lastnameCell);
+      const firstnameCell = document.createElement("td");
+      firstnameCell.innerHTML = item.firstname;
+      tr.appendChild(firstnameCell);
 
-    const passwordCell = document.createElement("td");
-    passwordCell.innerHTML = user.password;
-    tr.appendChild(passwordCell);
+      const lastnameCell = document.createElement("td");
+      lastnameCell.innerHTML = item.lastname;
+      tr.appendChild(lastnameCell);
 
-    const adminCell = document.createElement("td");
-    adminCell.innerHTML = user.admin;
-    tr.appendChild(adminCell);
+      const passwordCell = document.createElement("td");
+      passwordCell.innerHTML = item.password;
+      tr.appendChild(passwordCell);
 
-    const del = document.createElement("td");
-    const deleteButton = document.createElement("button");
-    deleteButton.textContent = "Delete user";
-    deleteButton.dataset.email = user.email; // Store the email in the dataset
-    del.appendChild(deleteButton);
-    tr.appendChild(del);
+      const adminCell = document.createElement("td");
+      adminCell.innerHTML = item.admin;
+      tr.appendChild(adminCell);
 
-    table.appendChild(tr); // Append the table row to the table
-  });
+      const del = document.createElement("td");
+      const deleteButton = document.createElement("button");
+      deleteButton.textContent = "Delete user";
+      deleteButton.dataset.email = item.email; // Store the email in the dataset
+
+      deleteButton.addEventListener("click", async () => {
+        try {
+          if (!item.admin) {
+          // Inside the deleteUser event listener
+          console.log("Deleting user with email:", deleteButton.dataset.email);
+          const response = await fetch("/api/deleteUser", {
+            method: "DELETE",
+            body: JSON.stringify({
+              email: deleteButton.dataset.email,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+
+          if (!response.ok) {
+            const errorMessage = await response.text();
+            throw new Error(errorMessage);
+          }
+
+          // User deleted successfully
+          console.log("User deleted successfully");
+
+          // Remove the deleted row from the table
+          tr.remove();
+        } else {
+          // Show alert if admin tries to delete another admin
+          alert("You cannot delete another admin user.");
+        }
+        } catch (error) {
+          console.error("Error deleting user:", error.message);
+        }
+      });
+
+      del.appendChild(deleteButton);
+      tr.appendChild(del);
+
+      table.appendChild(tr); // Append the table row to the table
+    });
+  } catch (error) {
+    console.error("Error fetching users:", error.message);
+  }
 };
 
 fetchimages();
-
-document.addEventListener("click", async (e) => {
-  try {
-    if (e.target.tagName.toLowerCase() === "button" && e.target.textContent === "Delete user") {
-      const userEmail = e.target.dataset.email; // Get the stored email from the dataset
-      const response = await fetch("/api/deleteUser", {
-        method: "DELETE",
-        body: JSON.stringify({
-          email: userEmail, // Use the captured user's email for deletion
-        }),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-
-      if (!response.ok) {
-        const errorMessage = await response.text();
-        throw new Error(errorMessage);
-      }
-
-      // User deleted successfully
-      console.log("User deleted successfully");
-
-      // Remove the deleted row from the table
-      const row = e.target.closest("tr");
-      row.remove();
-    }
-  } catch (error) {
-    console.error("Error deleting user:", error.message);
-  }
-});
--- src/routes/delete.js
+++ src/routes/delete.js
@@ -19,8 +19,9 @@ router.delete("/api/deleteProduct", async (req, res) => {
 });
 
 router.delete("/api/deleteUser", async (req, res) => {
-  const { name } = req.body;
-  const isExist = await User.deleteOne({ name });
+  const { email } = req.body;
+  // Inside the deleteUser route
+  const isExist = await User.deleteOne({ email });
 
   if (!isExist) {
     return res.send("User doesnt exists!!!!!!");
